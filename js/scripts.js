d68179db4e41b59c307a5658ee2b7e74aae95c53
Simon Stenström <simon.stenstrom.013@gmail.com>
2023-10-24 11:28:42 +0200

    added animations + closing cookie question

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 const hamburgerBtn = document.querySelector("#hamburger-btn");
 const navMenu = document.querySelector(".nav-menu");
+const cookiesYes = document.querySelector("#cookies-yes");
+const cookiesNo = document.querySelector("#cookies-no");
+const footer = document.querySelector("footer");
 
 hamburgerBtn.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu-open");
   hamburgerBtn.classList.toggle("active");
+});
+
+cookiesNo.addEventListener("click", () => {
+  footer.classList.toggle("footer-closed");
+});
+cookiesYes.addEventListener("click", () => {
+  footer.classList.toggle("footer-closed");
 });
